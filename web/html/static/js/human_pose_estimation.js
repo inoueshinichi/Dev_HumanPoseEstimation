7f0a72986e4d31f81f717ccd922bbd65dd261354
Inoue Shinichi Macbook Pro 2016 <inoue.shinichi.1800@gmail.com>
2023-07-10 02:33:43 +0900

    [Update] UI

--- web/html/static/js/human_pose_estimation.js
+++ web/html/static/js/human_pose_estimation.js
@@ -1,8 +1,35 @@
 'use strict';
 
+/* 
+クッキーを動かすにはサーバー環境が必要です。
+ファイルスキーム(file:///)では動きません。
+*/
+const agree = Cookies.get('cookie-agree');
+console.log('agree ', agree);
+if (agree === 'yes') {
+    alert('Cookieを受け入れました');
+} else {
+    alert('Cookieを確認できません');
+}
+
 $(function() {
 
-    $('#target').change(function () {
+    // drawer show
+    $('#open_nav').click(function () {
+        $('#nav').toggleClass('show');
+    });
+
+    // drawer hidden
+    $('#nav').click(function () {
+        $('#nav').toggleClass('show');
+    });
+
+    $('#input_image').change(function () {
+
+        $('#inference').children().each(function () {
+            $(this).remove()
+        })
+
         // フォームで選択された全ファイルを取得
         var fileList = this.files;
 
@@ -15,11 +42,11 @@ $(function() {
             var view = '<a href="' + blobUrl + '" target="_blank"><img src="' + blobUrl + '"></a>';
             var figText = '入力画像'
             var caption = `<figcaption>${figText}</figcaption>`
-            var frame = `<div>${view}${caption}</div>`
+            var frame = `<div class="input_block">${view}${caption}</div>`
             var item = `<figure id="input_view">${frame}</figure>`;
             console.log('item', item);
             
-            $('#inference').append(item);
+            $('#inference').append(item).append(item);
 
         }
     });
