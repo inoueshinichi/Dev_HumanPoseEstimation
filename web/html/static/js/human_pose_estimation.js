813dac2b66f9901007242a052b5a0b5edaae348c
Inoue Shinichi Macbook Pro 2016 <inoue.shinichi.1800@gmail.com>
2023-07-10 20:26:55 +0900

    [Update] Finish UI

--- web/html/static/js/human_pose_estimation.js
+++ web/html/static/js/human_pose_estimation.js
@@ -1,53 +1,86 @@
 'use strict';
 
+const isDebug = true;
+
 /* 
 クッキーを動かすにはサーバー環境が必要です。
 ファイルスキーム(file:///)では動きません。
 */
+const cookie_expires = 1; // 1日
 const agree = Cookies.get('cookie-agree');
 console.log('agree ', agree);
 if (agree === 'yes') {
-    alert('Cookieを受け入れました');
+    // alert('Cookieを受け入れました');
+    console.log('Cookieを受けれました.');
 } else {
-    alert('Cookieを確認できません');
+    // alert('Cookieを確認できません');
+    console.log('Cookieを確認できません.')
 }
 
 $(function() {
 
-    // drawer show
+    /* Cookie */
+    $('#btn_cookie_agree').click(function () {
+        Cookies.set('cookie-agree', 'yes', { expires: cookie_expires });
+        if (isDebug) {
+            console.log("Cookieを作成しました.");
+        }
+        $('#cookie_panel').remove();
+    });
+
+    $('#btn_cookie_reject').click(function () {
+        Cookies.remove('cookie-agree');
+        if (isDebug) {
+            console.log("Cookieを削除または拒否しました.");
+        }
+        $('#cookie_panel').remove();
+    });
+
+    /* Drawer Nav */
+    // show
     $('#open_nav').click(function () {
         $('#nav').toggleClass('show');
     });
-
-    // drawer hidden
+    // hidden
     $('#nav').click(function () {
         $('#nav').toggleClass('show');
     });
 
+    /* Set images */
     $('#input_image').change(function () {
 
         $('#inference').children().each(function () {
             $(this).remove()
-        })
+        });
 
         // フォームで選択された全ファイルを取得
         var fileList = this.files;
 
         // 個数分の画像を表示する
-        for (var i = 0, l = fileList.length; l > i; i++) {
+        for (var i = 0, len = fileList.length; len > i; i++) {
+
             // Blob URLの作成
             var blobUrl = window.URL.createObjectURL(fileList[i]);
 
             // HTMLに書き出し (src属性にblob URLを指定)
-            var view = '<a href="' + blobUrl + '" target="_blank"><img src="' + blobUrl + '"></a>';
-            var figText = '入力画像'
-            var caption = `<figcaption>${figText}</figcaption>`
-            var frame = `<div class="input_block">${view}${caption}</div>`
-            var item = `<figure id="input_view">${frame}</figure>`;
-            console.log('item', item);
-            
-            $('#inference').append(item).append(item);
-
+            var view = `<a href=${blobUrl} target="_blank"><img src="${blobUrl}" alt="入力画像_${i}"></a>`;
+            var figure = `<figure class="input_view">${view}</figure>`;
+            var caption = `<figcaption>入力画像_${i}</figcaptioin>`;
+            var item1 = `<div class="col-sm-6"><div class="input">${figure}${caption}</div></div>`;
+            // var item2 = `<span class="output">${figure}${caption}</span>`;
+            var block = `<div class="row">${item1}</div>`
+            var list = `<li id="item_list_${i}">${block}</li>`
+            console.log('list', list);
+            $('#inference').append(list);
         }
+
+        // 姿勢推定結果の挿入(仮)
+        // var index = 0
+        // var blobUrl = window.URL.createObjectURL(fileList[index]);
+        // var view = `<a href=${blobUrl} target="_blank"><img src="${blobUrl}" alt="入力画像_${index}"></a>`;
+        // var figure = `<figure class="input_view">${view}</figure>`;
+        // var caption = `<figcaption>姿勢推定画像_${index}</figcaptioin>`
+        // var result = `<div class="col-sm-6"><div class="output">${figure}${caption}</div></div>`
+        // $(`#item_list_${index}`).children('.row').append(result);
     });
 });
