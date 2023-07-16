c9f168447ac3f8e6f93968c251cb7a44f961b096
Inoue Shinichi Macbook Pro 2016 <inoue.shinichi.1800@gmail.com>
2023-07-16 13:13:04 +0900

    [Update] construct docker containers

---
+++ app/src/web_api_srv.py
@@ -0,0 +1,252 @@
+"""姿勢推定用のWebAPIサーバー
+"""
+from email.charset import BASE64
+import os
+import sys
+
+# os.sepはプラットフォーム固有の区切り文字(Windows: `\`, Unix: `/`)
+module_parent_dir = os.sep.join([os.path.dirname(__file__), '..'])
+print("module_parent_dir", module_parent_dir)
+sys.path.append(module_parent_dir)
+
+import re
+import io
+import json
+import asyncio
+import itertools
+import functools
+import threading
+import base64
+
+import redis
+import PIL.Image
+import numpy as np
+from flask import (
+    Flask, 
+    jsonify, 
+    request,
+)
+
+
+
+import torch
+import torchvision
+
+# import sanic
+# from sanic import Sanic
+# from sanic.response import (
+#     json,
+#     text,
+# )
+# from sanic.log import logger
+# from sanic.exceptions import ServerError
+
+
+from type_hint import *
+from log_conf import logging
+
+log = logging.getLogger(__name__)
+
+# log.setLevel(logging.WARN)
+log.setLevel(logging.INFO)
+# log.setLevel(logging.DEBUG)
+
+app = Flask(__name__)
+
+
+# Redis db conf
+REDIS_HOST: str = os.environ['REDIS_HOST']
+REDIS_PORT: int = int(os.environ['REDIS_PORT'])
+REDIS_DB: int = int(os.environ['REDIS_DB'])
+
+print('---- Check db access setting ----')
+print('REDIS_HOST', REDIS_HOST)
+print('REDIS_PORT', REDIS_PORT)
+print('REDIS_DB', REDIS_DB)
+
+print('Connect redis db server')
+REDIS = redis.Redis(host=REDIS_HOST, 
+                    port=REDIS_PORT,
+                    db=REDIS_DB,
+                    )
+print('Redis db server: ', REDIS)
+
+# App port (for this REST_API server)
+APP_PORT = int(os.environ['APP_PORT'])
+
+# REST_API server
+app = Flask("rest-api app server")
+
+# 日本語を使えるように
+app.config['JSON_AS_ASCII'] = False
+
+API_VER = 'v1.0'
+DB_STORE_COUNT = 1000000 # 100万
+
+# POSTで公開
+@app.route("/api/{API_VER}/pose_estimate/predict", methods=["POST"])
+def predict():
+    # content_type: str = request.headers.get('Content-Type')
+    # if content_type == 'application/json':
+
+    if not request.is_json:
+        return error(400.4)
+
+    json_data = request.get_json() # POSTされたjsonを取得
+    json = json.loads(json_data) # jsonを辞書に変換
+    id: int = json['id']
+    hostname: str = json['hostname']
+    ipv4: str = json['ipv4']
+    ipv6: str = json['ipv6']
+    first_name: str = json['first_name']
+    last_name: str = json['last_name']
+    age: int = json['age']
+    gender: str = json['gender'] # '男':0, '女':1, 'その他':2
+    gender_category = {
+        0: '男',
+        1: '女', 
+        2: 'その他'
+    }
+
+    response_list = []
+    images: Any = json['images']
+    for ndx, content in enumerate(images):
+        mata = content['meta']
+        shape = mata['shape']
+        is_first_channel: bool = mata['first-channel']
+        img_str = content['data'] # base64 encoded image string
+        img_base64 = img_str.encode('utf-8') # base64 encoded binary
+        img_bytes = base64.decodebytes(img_base64) # raw binary
+        buffer = io.BytesIO(img_base64)
+        img_np = np.frombuffer(buffer=buffer.getvalue(), dtype=np.uint8)
+
+        # numpy -> tensor
+        in_tensor = torch.from_numpy(img_np)
+        in_tensor = in_tensor.view(*shape)
+        in_tensor = in_tensor.to(torch.float32)
+        in_tensor = (in_tensor - in_tensor.min()) / (in_tensor.max() - in_tensor.min()) # [0,255] -> [0,1]
+
+        # 推論処理
+
+        buffer = io.BytesIO(bytearray(img_np)) # np.uint8
+        img_bytes = buffer.getvalue() # bytes
+        img_base64 = base64.encodebytes(img_bytes) # base64 encoded image binary
+        img_str = img_base64.decode('utf-8') # base64 encoded image string
+
+        json_element = {
+            'index' : ndx,
+            'data': img_str
+        }
+
+        response_list.append(json_element)
+
+    
+    # json.dumps(,indent=4)
+
+    jsonify({"images": response_list})
+
+
+@app.route(f'/api/{API_VER}/keys/', methods=['GET'])
+def api_keys():
+    data = {}
+    cursor = '0'
+    while cursor != 0:
+        cursor, keys = REDIS.scan(cursor=cursor,
+                                  count=DB_STORE_COUNT,
+                                  )
+        if len(keys) == 0:
+            break
+        keys = [key.decode() for key in keys]
+        values = [value.decode() for value in REDIS.mget(*keys)]
+        data.update(dict(zip(keys, values)))
+    
+    return success(data)
+
+
+@app.route(f'/api/{API_VER}/keys/<key>', methods=['GET', 'POST', 'PUT', 'DELETE'])
+def api_key(key):
+    if not isalnum(key):
+        print('key is not isalnum')
+        return error(400.1)
+    
+    body = request.get_data().decode().strip()
+    if request.method in ['POST', 'PUT']:
+        if body == '':
+            print('body empty')
+            return error(400.2)
+        if not isalnum(body):
+            print('body is not alnum')
+            return error(400.3)
+    
+    def get():
+        value = REDIS.get(key)
+        if value is not None: # valueが空
+            return success({key:value.decode()})
+        return error(404)
+    
+    def post():
+        if REDIS.get(key) is not None: # 既にkeyが存在
+            return error(409)
+        REDIS.set(key, body)
+        return success({key:body})
+    
+    def put():
+        REDIS.set(key, body)
+        return success({key:body})
+    
+    def delete():
+        if REDIS.delete(key) == 0: # keyが空
+            return error(404)
+        return success({})
+    
+    func_dict = {
+        'GET': get,
+        'POST': post,
+        'PUT': put,
+        'DELETE': delete,
+    }
+    return func_dict[request.method]() # HTTPのAPIに応じてRedisとの仲介を行う
+
+
+def isalnum(text):
+    # 半角英数字で構成された文字列にマッチした場合, Trueを返す
+    return re.match(r'^[a-zA-Z0-9]+$', text) is not None
+
+def success(kv):
+    # ブラウザにJson形式で値を返す
+    return jsonify(kv), 200 # HTTPレスポンスコード 200 : 成功
+
+def error(code):
+    message = {
+        400.1: "bad request. key must be alnum",
+        400.2: "bad request. post/put needs value on body",
+        400.3: "bad request. value must be alnum",
+        400.4: "bad request. content-type must be application/json",
+        404: "request not found",
+        409: "resource conflict. resource already exist",
+    }
+    return jsonify({'error': message[code], 'code': int(code)}), int(code)
+
+
+# 404 レスポンスコード(アクセスデータが存在しない) ハンドラ
+@app.errorhandler(404)
+def api_not_found_error(error):
+    return jsonify({'error': "api not found", 'code': 404}), 404
+
+# 405 レスポンスコード(アクセス権限) ハンドラ
+@app.errorhandler(405)
+def method_not_allowed_error(error):
+    return jsonify({'error': "method not allowed", 'code': 405}), 405
+
+# 500 レスポンスコード(サーバー内部エラー) ハンドラ
+@app.errorhandler(500)
+def internal_server_error(error):
+    return jsonify({'error': 'server internal error', 'code': 500}), 500
+
+
+if __name__ == "__main__":
+    print('[Start] flask app server')
+    app.run(debug=True,
+            host='0.0.0.0',
+            port=APP_PORT,
+            )
--- web/html/static/js/human_pose_estimation.js
+++ web/html/static/js/human_pose_estimation.js
@@ -17,70 +17,220 @@ if (agree === 'yes') {
     console.log('Cookieを確認できません.')
 }
 
-$(function() {
+/* Image & attribute array (shape(C,H,W), blobURL) */
+let imageAttribList = [];
 
-    /* Cookie */
-    $('#btn_cookie_agree').click(function () {
+/* Cookie */
+document.getElementById('btn_cookie_agree').addEventListener(
+    'click',
+    function () {
         Cookies.set('cookie-agree', 'yes', { expires: cookie_expires });
         if (isDebug) {
             console.log("Cookieを作成しました.");
         }
-        $('#cookie_panel').remove();
-    });
+        document.getElementById('cookie_panel').remove()
+    }
+);
 
-    $('#btn_cookie_reject').click(function () {
+document.getElementById('btn_cookie_reject').addEventListener(
+    'click',
+    function () {
         Cookies.remove('cookie-agree');
         if (isDebug) {
             console.log("Cookieを削除または拒否しました.");
         }
-        $('#cookie_panel').remove();
-    });
+        document.getElementById('cookie_panel').remove();
+    }
+);
 
-    /* Drawer Nav */
-    // show
-    $('#open_nav').click(function () {
-        $('#nav').toggleClass('show');
-    });
-    // hidden
-    $('#nav').click(function () {
-        $('#nav').toggleClass('show');
-    });
 
-    /* Set images */
-    $('#input_image').change(function () {
+/* Drawer Navigation */
+document.getElementById('open_nav').addEventListener(
+    'click',
+    function () {
+        // show
+        const item = document.getElementById('nav');
+        if (item.classList.contains("show")) {
+            item.classList.remove("show");
+        } else {
+            item.classList.add("show");
+        }
+    }
+);
 
-        $('#inference').children().each(function () {
-            $(this).remove()
-        });
+document.getElementById('nav').addEventListener(
+    'click',
+    function () {
+        // hide
+        const item = document.getElementById('nav');
+        if (item.classList.contains("show")) {
+            item.classList.remove("show");
+        }
+    }
+);
+
+
+/* Set images */
+document.getElementById('input_image').addEventListener(
+    'change',
+    async function () {
+
+        // 既存画像リストを削除
+        let item = document.getElementById('inference').firstChild;
+        while (item.firstChild) {
+            item.removeChild(item.firstChild);
+        }
+        imageAttribList.length = 0; // 配列全消し
+
 
         // フォームで選択された全ファイルを取得
-        var fileList = this.files;
+        let fileList = this.files;
 
+        console.log('fileList', fileList);
+        
         // 個数分の画像を表示する
-        for (var i = 0, len = fileList.length; len > i; i++) {
+        for (var i = 0; i < fileList.length; i++) {
+
+            // ファイル名を取得
+            let fileName = fileList[i].name;
 
             // Blob URLの作成
-            var blobUrl = window.URL.createObjectURL(fileList[i]);
+            let blobURL = window.URL.createObjectURL(fileList[i]);
+            console.log('input blobURL', blobURL);
 
+            // 画像サイズの取得
+            const image = new Image();
+            image.addEventListener('load', (event) => {
+                // 非同期処理
+                imageAttribList.push({
+                    'channel': 4, // RGBA (maybe)
+                    'height': image.naturalHeight,
+                    'width': image.naturalWidth,
+                    'type': image.type,
+                    'blob_url': image.src, // blobURL
+                    'file': fileList[i]
+                });
+
+                console.log('imageAttribList ', imageAttribList);
+            });
+            image.src = blobURL; // イベントトリガー
+            // await new Promise(resolve => image.load = () => resolve()); // 同期処理のため待機
+            // console.log("promise result", image.result);
+
+            // imageAttribList.push({
+            //     'channel': 4, // RGBA (maybe)
+            //     'height': image.naturalHeight,
+            //     'width': image.naturalWidth,
+            //     'blob_url': image.src, // blobURL
+            //     'file': fileList[i]
+            // });
+
+            
             // HTMLに書き出し (src属性にblob URLを指定)
-            var view = `<a href=${blobUrl} target="_blank"><img src="${blobUrl}" alt="入力画像_${i}"></a>`;
-            var figure = `<figure class="input_view">${view}</figure>`;
-            var caption = `<figcaption>入力画像_${i}</figcaptioin>`;
-            var item1 = `<div class="col-sm-6"><div class="input">${figure}${caption}</div></div>`;
-            // var item2 = `<span class="output">${figure}${caption}</span>`;
-            var block = `<div class="row">${item1}</div>`
-            var list = `<li id="item_list_${i}">${block}</li>`
-            console.log('list', list);
-            $('#inference').append(list);
+            const linkImg = document.createElement('a');
+            linkImg.setAttribute('href', `${blobURL}`);
+            linkImg.setAttribute('target', '_blank');
+
+            const img = document.createElement('img');
+            img.setAttribute('src', `${blobURL}`);
+            img.setAttribute('alt', `入力画像_${i}`)
+
+            linkImg.appendChild(img);
+
+            const figure = document.createElement('figure');
+            figure.setAttribute('class', 'input_view');
+            figure.appendChild(linkImg);
+
+            const figcaption = document.createElement('figcaption');
+            const caption = document.createTextNode(`入力画像_${i}`);
+            figcaption.appendChild(caption);
+
+            const inputOutlineDiv = document.createElement('div') ;
+            inputOutlineDiv.setAttribute('class', 'col-sm-6');
+            const inputDiv = document.createElement('div');
+            inputDiv.setAttribute('class', 'input');
+
+            const filename = document.createElement('figcaption');
+            filename.setAttribute('class', 'filename');
+            filename.innerHTML = `${fileName}`;
+            inputDiv.appendChild(filename);
+            inputDiv.appendChild(figure);
+            inputDiv.appendChild(figcaption);
+            inputOutlineDiv.appendChild(inputDiv);
+
+            const blockDiv = document.createElement('div');
+            blockDiv.setAttribute('class', 'row');
+            blockDiv.appendChild(inputOutlineDiv);
+
+            const listRecord = document.createElement('li');
+            listRecord.setAttribute('id', `item_list_${i}`);
+            listRecord.appendChild(blockDiv);
+
+            console.log('list', listRecord);
+            document.getElementById('inference').appendChild(listRecord);
+        }
+    }
+);
+
+/* Inference request to web api server */
+document.getElementById('inference_button').addEventListener(
+    'click',
+    async function () {
+        const firstName = document.getElementById('input_first_name').value;
+        const lastName = document.getElementById('input_last_name').value;
+        const age = document.getElementById('input_age').value;
+
+        const genderChoice = document.querySelector('input[name="gender"]:checked');
+        let gender;
+        if (genderChoice !== null) {
+            gender = genderChoice.value;
+        } else {
+            gender = "";
         }
 
-        // 姿勢推定結果の挿入(仮)
-        // var index = 0
-        // var blobUrl = window.URL.createObjectURL(fileList[index]);
-        // var view = `<a href=${blobUrl} target="_blank"><img src="${blobUrl}" alt="入力画像_${index}"></a>`;
-        // var figure = `<figure class="input_view">${view}</figure>`;
-        // var caption = `<figcaption>姿勢推定画像_${index}</figcaptioin>`
-        // var result = `<div class="col-sm-6"><div class="output">${figure}${caption}</div></div>`
-        // $(`#item_list_${index}`).children('.row').append(result);
-    });
-});
+        console.log(`first: ${firstName}, last: ${lastName}, age: ${age}, gender: ${gender}`);
+
+        let postData = {};
+        postData['first_name'] = firstName;
+        postData['last_name'] = lastName;
+        postData['age'] = age;
+        postData['gender'] = gender; // 0: 男性, 1: 女性, 2: その他
+        postData['images'] = []
+
+        console.log('imageAttribList: ', imageAttribList);
+
+
+        for (let i = 0; i < imageAttribList.length; i++) {
+            const channel = imageAttribList[i].channel;
+            const height = imageAttribList[i].height;
+            const width = imageAttribList[i].width;
+            const blobURL = imageAttribList[i].blob;
+            const filename = blobURL.name;
+            const filetype = blobURL.type;
+
+            console.log('output blobURL', blobURL)
+            
+            // dataURLに変換(同期処理)
+            const reader = new FileReader();
+            reader.readAsDataURL(blobURL);
+            await new Promise(resolve => reader.onload = () => resolve()); // 同期処理のため待機
+
+            const base64DataURL = reader.result;
+            const base64Str = base64DataURL.split(',')[1];
+
+            const value = {
+                'meta': {
+                    'filename': filename,
+                    'type': filetype,
+                    'shape': [channel, height, width]
+                },
+                'data': base64Str
+            };
+
+            postData['images'].push(value);
+        }
+
+        console.log('postData: ', postData);
+        const jsonData = JSON.stringify(postData);
+    }
+);
